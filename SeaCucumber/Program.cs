7bce15fb9b09aa0276f16234de0cb35c06a009cb
JakubCph <38160647+JakubCph@users.noreply.github.com>
2022-02-07 19:03:45 +0100

    Implemented crab position calculation to minimize fuel consumption

--- SeaCucumber/Program.cs
+++ SeaCucumber/Program.cs
@@ -3,57 +3,61 @@ using SonarSweep;
 using SonarSweep.BingoGame;
 using SonarSweep.HydrothermalVenture;
 using SonarSweep.LanthernFish;
+using SonarSweep.TreacheryOfWhales;
 using static SonarSweep.Pilot;
 
-Console.WriteLine("Hello, World!");
+//IList<int> inputDepths = new List<int>();
+//foreach (var line in File.ReadLines(@"DepthsScanReport.txt"))
+//{
+//    inputDepths.Add(int.Parse(line));   
+//}
+//int numberOfIncreasedDepths = Calculate3MeasurementSlidingWindow(inputDepths).Count(e => e.Equals(DepthChange.Increased));
 
-IList<int> inputDepths = new List<int>();
-foreach (var line in File.ReadLines(@"DepthsScanReport.txt"))
-{
-    inputDepths.Add(int.Parse(line));   
-}
-int numberOfIncreasedDepths = Calculate3MeasurementSlidingWindow(inputDepths).Count(e => e.Equals(DepthChange.Increased));
+//Console.WriteLine($"Increased depths: {numberOfIncreasedDepths}");
 
-Console.WriteLine($"Increased depths: {numberOfIncreasedDepths}");
+///* Sonar Sweep */
+//var pilot = new Pilot();
+//foreach(string? line in File.ReadLines(@"PlannedCourse.txt"))
+//{
+//    if(line is not null)
+//    {
+//        var parts= line.Split(' ');
+//        Direction direction = (Direction) Enum.Parse(typeof(Direction), parts[0]);
+//        int step = int.Parse(parts[1]);
 
-/* Sonar Sweep */
-var pilot = new Pilot();
-foreach(string? line in File.ReadLines(@"PlannedCourse.txt"))
-{
-    if(line is not null)
-    {
-        var parts= line.Split(' ');
-        Direction direction = (Direction) Enum.Parse(typeof(Direction), parts[0]);
-        int step = int.Parse(parts[1]);
-
-        pilot.Move2(direction, step);
-    }
-}
-Console.WriteLine($"Horizontal position * depth = {pilot.HorizontalPosition * pilot.Depth}");
+//        pilot.Move2(direction, step);
+//    }
+//}
+//Console.WriteLine($"Horizontal position * depth = {pilot.HorizontalPosition * pilot.Depth}");
 
 
-var diag = new Diagnostic();
-diag.CalculateParameters();
-Console.WriteLine($"Power consumption is {diag.Gamma * diag.Epsilon}");
+//var diag = new Diagnostic();
+//diag.CalculateParameters();
+//Console.WriteLine($"Power consumption is {diag.Gamma * diag.Epsilon}");
 
-var bingo = new BingoGame();
-var result = bingo.Play();
-Console.WriteLine($"The winning board scored {result}");
+//var bingo = new BingoGame();
+//var result = bingo.Play();
+//Console.WriteLine($"The winning board scored {result}");
 
-bingo = new BingoGame();
-result = bingo.PlayForLastWinningBoard();
-Console.WriteLine($"The last winning board scored {result}");
+//bingo = new BingoGame();
+//result = bingo.PlayForLastWinningBoard();
+//Console.WriteLine($"The last winning board scored {result}");
 
-var ventsMap = new VentsMap();
-var overlapping = ventsMap.CalcOverlappingLines();
-Console.WriteLine($"Overlapping vents are {overlapping}");
+//var ventsMap = new VentsMap();
+//var overlapping = ventsMap.CalcOverlappingLines();
+//Console.WriteLine($"Overlapping vents are {overlapping}");
 
-var fishSchool = new FishSimulation();
-fishSchool.SimulateForDays(80);
-fishSchool.SimulateForDays(80);
-Console.WriteLine($"After 80 days is {fishSchool.Count}");
-fishSchool.SimulateForLongDays(256);
-Console.WriteLine($"After 256 days is {fishSchool.CountLongSimulation}");
+//var fishSchool = new FishSimulation();
+//fishSchool.SimulateForDays(80);
+//fishSchool.SimulateForDays(80);
+//Console.WriteLine($"After 80 days is {fishSchool.Count}");
+//fishSchool.SimulateForLongDays(256);
+//Console.WriteLine($"After 256 days is {fishSchool.CountLongSimulation}");
+
+var crabSim = new CrabSimulation();
+var res = crabSim.CalcHorizontalPos();
+Console.WriteLine($"Min fuel expense at {res.fuel}, happens for position: {res.position}");
+
 
 static IList<bool>? CalculateIncreasedDepths(IList<int> depths)
 {
---
+++ SeaCucumber/TreacheryOfWhales/CrabSimulation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarSweep.TreacheryOfWhales
+{
+    public class CrabSimulation
+    {
+        public ushort[] CrabPositions { get; }
+        private string path = @"TreacheryOfWhales\WhalesPositions.txt";
+        public CrabSimulation()
+        {
+            CrabPositions = File.ReadAllText(path).Split(",").Select(s => ushort.Parse(s)).ToArray();
+        }
+        /// <summary>
+        /// Fuel efficient horizontal position
+        /// </summary>
+        public (int position, int fuel) CalcHorizontalPos()
+        {
+            ushort maxPos = 0;
+            int sumPrev = 0; 
+            foreach (var pos in CrabPositions)
+            {
+                if(pos > maxPos) maxPos = pos;
+
+                sumPrev += calculateFuel(pos);
+            }
+
+            int sumCurr = 0;
+            for (int optimalPos = 1; optimalPos <= maxPos; optimalPos++)
+            {
+                for (int j = 0; j < CrabPositions.Length; j++)
+                {
+                    var posDiff = Math.Abs(CrabPositions[j] - optimalPos);
+                    sumCurr += calculateFuel(posDiff);
+                }
+                
+                if (sumCurr > sumPrev) 
+                {
+                    return (optimalPos - 1, sumPrev); 
+                }
+                else
+                {
+                    sumPrev = sumCurr;
+                    sumCurr = 0;
+                }
+            }
+            return (0,0);
+        }
+        private int calculateFuel(int posDiff)
+        {
+            return posDiff * (1 + posDiff) / 2;
+        }
+    }
+}
